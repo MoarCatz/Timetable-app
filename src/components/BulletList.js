a8c85c34b9aed9e7fe0de5dce6d70b9ee583cbd7
Leva7 <leva181777@gmail.com>
2017-11-06 20:38:57 +0500

    :lipstick: Add BulletList component

---
+++ src/components/BulletList.js
@@ -0,0 +1,56 @@
+'use strict';
+import React, { Component } from 'react';
+import {
+  View,
+  StyleSheet,
+  FlatList
+} from 'react-native';
+import PropTypes from 'prop-types';
+import {
+  MaterialText,
+  withMaterialStyles
+} from 'material-native';
+
+export class BulletList extends Component {
+  static propTypes = {
+    rows: PropTypes.array
+  };
+
+  renderRow({item}) {
+    return (
+      <View style={styles.row}>
+        <MaterialText style={styles.bullet}>●</MaterialText>
+        <MaterialText>{item}</MaterialText>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <FlatList
+        data={this.props.rows}
+        renderItem={this.renderRow}
+        keyExtractor={(item, index) => index}
+        style={styles.list}
+      />
+    )
+  }
+}
+
+export default withMaterialStyles((materialTheme) => ({}))(BulletList);
+
+export const styles = StyleSheet.create({
+  list: {
+    marginLeft: 10,
+    marginVertical: 5
+  },
+  row: {
+    flexDirection: 'row',
+    marginVertical: 1
+  },
+  bullet: {
+    alignSelf: 'flex-start',
+    flex: 0,
+    marginRight: 5
+  }
+})
--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export {default as Tag} from './Tag';
 export {default as TimetableList} from './TimetableList';
 export {default as TeacherTimetableList} from './TeacherTimetableList';
 export {default as Card} from './Card';
+export {default as BulletList} from './BulletList';
